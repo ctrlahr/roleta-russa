1f942520bf3012d22f3927dc728485c6b2d9e9fe
) <ctrlahrium@gmail.com>
2024-01-14 17:39:23 -0300

    atualização da roleta russa
    deixei o sistema melhor, com repetições e tratamentos de erros!!

--- roleta.py
+++ roleta.py
@@ -1,26 +1,46 @@
 from random import choice
+numeros = [1, 2, 3, 4, 5, 6]
+def linha(l):
+    print('-' * 25)
+    return l
 
-print('=' * 30)
-print('ROLETA RUSSA')
-print('=' * 30)
+linha(linha)
+print('     ROLETA RUSSA     ')
+linha(linha)
 
-
-revolver = [1, 2, 3, 4, 5, 6]
-roleta = choice(revolver)
-jogador = int(input('Escolha um número de 1 até 6: '))
 
 while True:
-    if jogador == roleta:
-        resposta = str(input(('Você perdeu, quer tentar dnv? [Y/N]: '))).upper()[0]
-        if resposta == 'N':
-            break
-            
+# ESCOLHA DA ROLETA
+    revolver = choice(numeros)
+# DEFININDO OQ ACONTECE SE O NÚMERO FOR MAIOR QUE 6 OU MENOR QUE 1
+    try:
+        jogador = int(input('Escolha um número entre 1 e 6: '))
+        if jogador > 6 or jogador < 1:
+            print('Escolha um número entre 1 e 6 seu animal!')
+            continue
 
-#escolha = print('Quer tentar de novo? [Y/N]').upper
-#if escolha == 'Y':
+# SE O JOGADOR PERDEU
+        if jogador == revolver:
+            print('você perdeu!')
+            escolha = str(input('Quer tentar dnv? [S/N]: ')).upper()[0]
+            if escolha == 'Y':
+                continue
+            elif escolha == 'N':
+                print('Parabéns você saiu vivo! :)')
+                break
+            
+# SE O JOGADOR GANHOU
+        elif jogador != revolver:
+            print('Você ganhou!')
+            escolha2 = str(input(('Quer tentar morrer dnv? [S/N]: '))).upper()[0]
+            if escolha2 == 'Y':
+                continue
+            elif escolha2 == 'N':
+                print('Você saiu vivo, parabéns! :)')
+                break
     
-#else:
-#    print('O jogo acabou!')
-        
+    except:
+        print('O valor digitado não é um número inteiro!!')            
+
 
 
